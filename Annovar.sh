cead490c6509e3fe72e5cc1eeadb8a68bd6abba3
Sanjayan.A <a.sanjayan2002@gmail.com>
2025-05-23 17:52:57 +0530

    Add files via upload

---
+++ Annovar.sh
@@ -0,0 +1,4 @@
+
+
+
+./table_annovar.pl /mnt/NGS_work/vc_somatic/BJ/somatic/BJ_filt_som.vcf humandb/ -buildver hg38 -out BJ_annovar -protocol refGeneWithVer,cosmic101,avsnp151 -operation g,f,f -remove -nastring . -vcfinput
